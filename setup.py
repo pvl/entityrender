f7591ecdbfa997d63a76a23bb9e0ccd663161ae8
Pedro Lima <pedro.lima@gmail.com>
2022-05-04 17:02:53 +0100

    packaging changes

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import setuptools
 
 setuptools.setup(
     name="entityrender",
-    version="0.1.0",
+    version="0.2.0",
     author="Pedro Lima",
     author_email="pedro.lima@gmail.com",
-    description="Basic entity span highlighting in text for visualization in notebooks (adapted from Spacy)",
+    description="Basic entity span highlighting in text for visualization in notebooks",
     long_description_content_type="text/markdown",
     url="https://github.com/pvl/entityrender",
     packages=setuptools.find_packages(),
